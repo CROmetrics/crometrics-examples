dc6c0fa3fc3c700825e16c4e5e54c0265eb9ccb3
Tom Fuertes <tomfuertes@gmail.com>
2016-01-16 00:05:14 -0800

    force/safe refactor

--- load-scripts/v1.js
+++ load-scripts/v1.js
@@ -1,16 +1,17 @@
-var loadScript = function(location, callback){
+/* _optimizely_evaluate=force */
+var loadScript = function(location, callback) {
     var fileRef = document.createElement('script');
-    fileRef.setAttribute('type','text/javascript');
+    fileRef.setAttribute('type', 'text/javascript');
     if (callback) {
-        if (fileRef.readyState) {  // IE
+        if (fileRef.readyState) { // IE
             fileRef.onreadystatechange = function() {
                 if (fileRef.readyState == 'loaded' || fileRef.readyState == 'complete') {
                     fileRef.onreadystatechange = null;
                     callback();
                 }
             };
-        } else {  // Non-IE
-            fileRef.onload = function(){
+        } else { // Non-IE
+            fileRef.onload = function() {
                 callback();
             };
         }
@@ -19,9 +20,13 @@ var loadScript = function(location, callback){
     document.head.appendChild(fileRef);
 };
 loadScript('https://d1tvvvm2zy17cz.cloudfront.net/js/jquery.oncreate.min.js', function() {
-// Edit here
- window.$.onCreate('#sample_id_element', exampleFunction);
+    // Edit here
+    $(function() {
+        window.$.onCreate('#sample_id_element', exampleFunction);
+    });
 });
-function exampleFunction(sample_id_element){
-// Your element is present.  Let the magic happen baby!
+
+function exampleFunction(sample_id_element) {
+    // Your element is present.  Let the magic happen baby!
 }
+/* _optimizely_evaluate=safe */
